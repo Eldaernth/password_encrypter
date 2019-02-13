97a6b563294cae2dbae3344c842c796ae5a76b54
Joó Norbert <joonorbi12@gmail.com>
2019-02-13 10:15:30 +0100

    encrypt and add to file

--- enrypter.py
+++ enrypter.py
@@ -1,12 +1,14 @@
 def menu(passwords):
-    print("1.Show passwords\n2.Add new password\n3.Save in file")
+    print("1.Show passwords\n2.Add new password")
     choice = get_user_input("Enter a number: ")
     if choice == "1":
         show_passwords(read_from_file())
     elif choice == "2":
         passwords.append(crypting())
-    elif choice == "3":
         add_to_file(passwords)
+    else:
+        raise KeyError("Wrong key")
+        
 
 def get_user_input(label):
     return input(label)
@@ -14,33 +16,34 @@ def get_user_input(label):
 
 def add_to_file(passwords):
     stored_passwords = open("passwords.txt","r+")
-    for i in range(len(passwords)):
-        stored_passwords.write(passwords)
+    for i in passwords:
+        stored_passwords.write("\t".join(i) + "\n")
 
 
 def read_from_file():
-    stored_passwords = open("passwords.txt","r+")
+    stored_passwords = open("passwords.txt","r")
     return stored_passwords.readlines()
 
 def show_passwords(passwords):
-    for i in passwords:
-        print(i)
+    for line in passwords:
+        print(line)
 
 
 def crypting():
     list_of_letters = ["a", "b", "c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"k" ,
                         "l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" ,"z",
-                        "0","1","2","3","4","5","6","7","8","9"]
+                        "0","1","2","3","4","5","6","7","8","9"," "]
     password = get_user_input("Enter password: ")
     list_of_passwords = list(password)
     crypted_password = []
     for chars in list_of_passwords:
         crypted_password.append(list_of_letters[list_of_letters.index(chars) - 3])
-    return (password,"".join(crypted_password))
+    return [password,"".join(crypted_password)]
 
 def main():
     passwords = [x for x in read_from_file()]
-    menu(passwords)
+    while True:
+        menu(passwords)
     
 
 if __name__ == "__main__":
